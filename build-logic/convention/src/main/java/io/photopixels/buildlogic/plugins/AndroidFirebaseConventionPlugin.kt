00af9ffc2e9cef47e291bf6c63f85a88fd535789
Zhelyazko Zhelev <zhelyazko.zhelev@scalefocus.com>
2024-06-21 12:53:50 +0300

    CI test

--- build-logic/convention/src/main/java/io/photopixels/buildlogic/plugins/AndroidFirebaseConventionPlugin.kt
+++ build-logic/convention/src/main/java/io/photopixels/buildlogic/plugins/AndroidFirebaseConventionPlugin.kt
@@ -29,6 +29,7 @@ class AndroidFirebaseConventionPlugin : Plugin<Project> {
                     apply(findPluginId("firebaseAppDistribution"))
                 } else {
                     println("Firebase services gradle plugins are not enabled; missing google-services.json file.")
+                    println("Searched in[${file("google-services.json").absolutePath}]")
                 }
             }
 
