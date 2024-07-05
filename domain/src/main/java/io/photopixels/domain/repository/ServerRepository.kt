de183bc57d9f70e4730f95572541e23e24b5e7de
Kostadin Georgiev <kostadin.georgiev@scalefocus.com>
2024-07-05 16:19:36 +0300

    Feature/acc 1375 server screen (#28)
    * [AC-1375] Add support for typing server address using ip.addr + protocol + port
    
    * [AC-1375] Code cleanup
    
    * [AC-1375] Adding custom network config per build type allow/disallow http network traffic
    
    * [AC-1375] Code cleanup
    
    * [AC-1375] Configure android.yml file to build and upload releaseHttp.apk to Github artifacts
    
    * [AC-1375] Add releaseHttp build proguard configuration
    
    * [AC-1375] Handle http traffic not allowed error
    
    * [AC-1375] PR comments fixed
    
    * [AC-1375] PR comments fixed
    
    * [AC-1375] PR comments fixed
    
    * [AC-1375] Test commit for github artifacts
    
    * [AC-1375] Test commit for github artifacts
    
    ---------
    
    Co-authored-by: Kostadin Georgiev <>

--- domain/src/main/java/io/photopixels/domain/repository/ServerRepository.kt
+++ domain/src/main/java/io/photopixels/domain/repository/ServerRepository.kt
@@ -1,18 +1,19 @@
 package io.photopixels.domain.repository
 
 import io.photopixels.domain.base.Response
+import io.photopixels.domain.model.ServerAddress
 import io.photopixels.domain.model.ServerRevision
 import io.photopixels.domain.model.ServerStatus
 import kotlinx.coroutines.flow.Flow
 
 interface ServerRepository {
-    suspend fun getServerStatus(serverAddress: String): Flow<Response<ServerStatus>>
+    suspend fun getServerStatus(serverAddress: ServerAddress): Flow<Response<ServerStatus>>
 
     suspend fun getServerRevision(specificRevision: Int?): Flow<Response<ServerRevision>>
 
-    suspend fun setServerAddressToDataStore(serverAddress: String)
+    suspend fun setServerAddressToDataStore(serverAddress: ServerAddress)
 
-    suspend fun getServerAddress(): String?
+    suspend fun getServerAddress(): ServerAddress?
 
     suspend fun setServerVersionToDataStore(serverVersion: String)
 
